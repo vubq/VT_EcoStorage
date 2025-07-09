e3be249ca14ce7393f4ade55d97ac7fde961303b
vubq <vubq242002@gmail.com>
2025-07-09 17:38:21 +0700

    Update

--- src/main/java/vubq/warehouse_management/VT_EcoStorage/entities/Base.java
+++ src/main/java/vubq/warehouse_management/VT_EcoStorage/entities/Base.java
@@ -9,6 +9,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldNameConstants;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import vubq.warehouse_management.VT_EcoStorage.configs.securities.auths.CustomUserDetails;
 import vubq.warehouse_management.VT_EcoStorage.utils.dates.DateUtils;
 
 import java.util.Date;
@@ -29,20 +33,38 @@ public abstract class Base {
 
     private String creator;
 
+    private String creatorUsername;
+
     @Column(updatable = false)
     private Date createdAt;
 
     private String updater;
+
+    private String updaterUsername;
 
     private Date updatedAt;
 
     @PrePersist
     protected void onCreate() {
         this.createdAt = DateUtils.getCurrentTime();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null) {
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+            creatorUsername = customUserDetails.getUser().getUsername();
+            creator = customUserDetails.getUser().getId();
+        }
     }
 
     @PreUpdate
     protected void onUpdate() {
         this.updatedAt = DateUtils.getCurrentTime();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null) {
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+            updaterUsername = customUserDetails.getUser().getUsername();
+            updater = customUserDetails.getUser().getId();
+        }
     }
 }
